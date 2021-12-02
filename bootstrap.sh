4520b2ff10f322f2a3765849a55c3e502f050d8c
pminervini <p.minervini@gmail.com>
2021-12-02 13:41:56 +0100

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,28 +2,28 @@
 
 mkdir -p data/processed/features/
 
-wget -c complete_dev.txt -P data/processed/
-wget -c complete_test.txt -P data/processed/
-wget -c complete_train.txt -P data/processed/
-wget -c complete_types_dev.txt -P data/processed/
-wget -c complete_types_test.txt -P data/processed/
-wget -c complete_types_train.txt -P data/processed/
-wget -c entities.txt -P data/processed/
-wget -c entities_types.txt -P data/processed/
-wget -c features/ -P data/processed/
-wget -c relations.txt -P data/processed/
-wget -c relations_types.txt -P data/processed/
-wget -c triples_all.tsv -P data/processed/
-wget -c triples_dev.tsv -P data/processed/
-wget -c triples_test.tsv -P data/processed/
-wget -c triples_train.tsv -P data/processed/
-wget -c triples_types_dev.tsv -P data/processed/
-wget -c triples_types_test.tsv -P data/processed/
-wget -c triples_types_train.tsv -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/complete_dev.txt -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/complete_test.txt -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/complete_train.txt -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/complete_types_dev.txt -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/complete_types_test.txt -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/complete_types_train.txt -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/entities.txt -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/entities_types.txt -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/features/ -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/relations.txt -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/relations_types.txt -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/triples_all.tsv -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/triples_dev.tsv -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/triples_test.tsv -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/triples_train.tsv -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/triples_types_dev.tsv -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/triples_types_test.tsv -P data/processed/
+wget -c http://data.neuralnoise.com/amil/data/processed/triples_types_train.tsv -P data/processed/
 
-wget -c dev.pt -P data/processed/features/
-wget -c test.pt -P data/processed/features/
-wget -c train.pt -P data/processed/features/
-wget -c types_dev.pt -P data/processed/features/
-wget -c types_test.pt -P data/processed/features/
-wget -c types_train.pt -P data/processed/features/
+wget -c http://data.neuralnoise.com/amil/data/processed/features/dev.pt -P data/processed/features/
+wget -c http://data.neuralnoise.com/amil/data/processed/features/test.pt -P data/processed/features/
+wget -c http://data.neuralnoise.com/amil/data/processed/features/train.pt -P data/processed/features/
+wget -c http://data.neuralnoise.com/amil/data/processed/features/types_dev.pt -P data/processed/features/
+wget -c http://data.neuralnoise.com/amil/data/processed/features/types_test.pt -P data/processed/features/
+wget -c http://data.neuralnoise.com/amil/data/processed/features/types_train.pt -P data/processed/features/
